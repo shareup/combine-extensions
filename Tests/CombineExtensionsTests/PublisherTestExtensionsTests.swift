b08d9535294539af91f315899b7b90b52acfb716
Anthony Drendel <me@anthonydrendel.com>
2021-04-12 21:01:41 +0200

    Add test helpers that accept evaluation blocks instead of concrete values for output and failure

---
+++ Tests/CombineExtensionsTests/PublisherTestExtensionsTests.swift
@@ -0,0 +1,60 @@
+import Combine
+import XCTest
+import CombineTestExtensions
+
+class PublisherTestExtensionsTests: XCTestCase {
+    func testExpectOutputWithEvaluator() throws {
+        var ints = [1, 2, 3]
+        let evaluator = { (input: Int) -> OutputExpectation in
+            XCTAssertEqual(ints.removeFirst(), input)
+            return ints.isEmpty ? .finished : .moreExpected
+
+        }
+        let ex = ints.publisher.expectOutput(evaluator)
+
+        wait(for: [ex], timeout: 0.5)
+    }
+
+    func testExpectOutputAndFailureWithEvaluators() throws {
+        let subject = PassthroughSubject<Int, Err>()
+
+        var ints = [1, 2, 3]
+
+        let ex = subject
+            .receive(on: DispatchQueue(label: "test"))
+            .expectOutputAndFailure(
+                { (output) -> OutputExpectation in
+                    XCTAssertEqual(ints.removeFirst(), output)
+                    return ints.isEmpty ? .finished : .moreExpected
+                },
+                failureEvaluator: { XCTAssertEqual(.correct, $0) }
+            )
+
+        subject.send(1)
+        subject.send(2)
+        subject.send(3)
+        subject.send(completion: .failure(.correct))
+
+        wait(for: [ex], timeout: 0.5)
+    }
+
+    func testExpectFailureWithEvaluator() throws {
+        let subject = PassthroughSubject<Int, Err>()
+
+        let ex = subject
+            .receive(on: DispatchQueue(label: "test"))
+            .expectFailure(
+                { XCTAssertEqual(.correct, $0) },
+                failsOnOutput: true
+            )
+
+        subject.send(completion: .failure(.correct))
+
+        wait(for: [ex], timeout: 0.5)
+    }
+}
+
+private enum Err: Error, Equatable {
+    case correct
+    case wrong
+}
