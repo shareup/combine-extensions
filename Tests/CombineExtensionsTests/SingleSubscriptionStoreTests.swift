a9d1c7194527962d4693aa81630ca569659df552
Anthony Drendel <atdrendel@users.noreply.github.com>
2021-04-14 17:56:48 +0200

    Add SingleSubscriptionStore and tests (#8)

---
+++ Tests/CombineExtensionsTests/SingleSubscriptionStoreTests.swift
@@ -0,0 +1,59 @@
+import XCTest
+import Combine
+import CombineExtensions
+import CombineTestExtensions
+
+class SingleSubscriptionStoreTests: XCTestCase {
+    func testSingleSubscriptionStoreEquatableAndHashable() throws {
+        let one = SingleSubscriptionStore()
+        let two = SingleSubscriptionStore()
+
+        let sameAsOne = one
+
+        XCTAssertEqual(one, one)
+        XCTAssertEqual(one, sameAsOne)
+        XCTAssertEqual(one.hashValue, one.hashValue)
+        XCTAssertEqual(one.hashValue, sameAsOne.hashValue)
+
+        XCTAssertNotEqual(one, two)
+        XCTAssertNotEqual(one.hashValue, two.hashValue)
+
+        [1, 2, 3]
+            .publisher
+            .sink { _ in }
+            .store(in: sameAsOne)
+
+        XCTAssertEqual(one, sameAsOne)
+        XCTAssertEqual(one.hashValue, sameAsOne.hashValue)
+    }
+
+    func testStoringSubscriptionPreventsCancellation() throws {
+        let store = SingleSubscriptionStore()
+
+        let subject = PassthroughSubject<Int, Never>()
+
+        let receiveEx = expectation(description: "Should have received '1'")
+        receiveEx.assertForOverFulfill = true
+
+        let doNotReceiveEx = expectation(description: "Should not have received '2'")
+        doNotReceiveEx.isInverted = true
+
+        subject.sink { value in
+            if value == 1 {
+                receiveEx.fulfill()
+            } else if value == 2 {
+                doNotReceiveEx.fulfill()
+            } else {
+                XCTFail()
+            }
+        }
+        .store(in: store)
+
+        subject.send(1)
+        wait(for: [receiveEx], timeout: 2)
+
+        store.removeSubscription()
+        subject.send(2)
+        wait(for: [doNotReceiveEx], timeout: 0.1)
+    }
+}
